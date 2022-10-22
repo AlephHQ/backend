29bf6fc1cc73cfa72661f5fa5c77fb21127be1bd
modsoussi <mohameddsoussi@gmail.com>
2022-10-22 16:11:27 -0400

    http api is not what i need. imap client init

--- dovecot/helpers.go
+++ dovecot/helpers.go
@@ -1,7 +1,14 @@
 package dovecot
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+	"fmt"
+)
 
 func getBase64EncodedDoveadmAPIKey() string {
 	return base64.StdEncoding.EncodeToString([]byte(dovecotDoveadmAPIKey))
+}
+
+func getAPIAuthorizationHeader() string {
+	return fmt.Sprintf("X-Dovecot-API %s", getBase64EncodedDoveadmAPIKey())
 }
--- dovecot/list_commands.go
+++ dovecot/list_commands.go
@@ -1,22 +1,62 @@
 package dovecot
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
 const dovecotDoveadmAPIKey = "AjshkdaEkjaad8sA6261aSdsj"
+const apiURL = "http://modsoussi.com:7000/doveadm/v1"
 
 func ListCommands() {
-	url := "http://modsoussi.com:7000/doveadm/v1"
-	apiAuthorization := fmt.Sprintf("X-Dovecot-API %s", getBase64EncodedDoveadmAPIKey())
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	req, err := http.NewRequest(http.MethodGet, apiURL, nil)
 	if err != nil {
 		log.Panic(err)
 	}
-	req.Header.Add("Authorization", apiAuthorization)
+	req.Header.Add("Authorization", getAPIAuthorizationHeader())
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Println(string(body))
+}
+
+func FetchMail() {
+	jsonBody := `
+[
+	[
+		"fetch",
+		{
+			"user": "mo@modsoussi.com",
+			"field": [
+				"mailbox"
+			],
+			"query": [
+				"mailbox"
+			]
+		},
+		"tag1"
+	]
+]
+	`
+
+	req, err := http.NewRequest(http.MethodPost, apiURL, strings.NewReader(jsonBody))
+	if err != nil {
+		log.Panic(err)
+	}
+	req.Header.Add("Authorization", getAPIAuthorizationHeader())
+	req.Header.Add("Content-Type", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {

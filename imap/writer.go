132dbc5ff789ed9b8214e1493a4da106257561fe
modsoussi <mohameddsoussi@gmail.com>
2022-10-23 22:06:47 -0400

    minor change

--- imap/writer.go
+++ imap/writer.go
@@ -11,13 +11,13 @@ type Writer struct {
 	*bufio.Writer
 }
 
-func (w *Writer) WriteString(s string) (int, error) {
-	return w.Writer.WriteString(s + crlf)
-}
-
 func NewWriter(w io.Writer) *Writer {
 	wr := &Writer{}
 	wr.Writer = bufio.NewWriter(w)
 
 	return wr
+}
+
+func (w *Writer) WriteString(s string) (int, error) {
+	return w.Writer.WriteString(s + crlf)
 }

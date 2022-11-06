7fb69a07bb4e9a559c98f80c72f529becd546653
modsoussi <mohameddsoussi@gmail.com>
2022-11-06 13:49:55 -0500

    got started on Message handling

--- imap/message.go
+++ imap/message.go
@@ -1,5 +1,19 @@
 package imap
 
 type Message struct {
-	UID uint32
+	UID uint64
+
+	SeqNum uint64
+
+	Flags map[string]bool
+
+	InternalDate string
+
+	Size uint64
+
+	Envelope interface{}
+
+	Body interface{}
+
+	Text string
 }

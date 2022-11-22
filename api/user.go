fcdc84226fd53bb3d4e041dd1927af2645bb8102
modsoussi <mohameddsoussi@gmail.com>
2022-11-21 19:49:11 -0500

    fixed user json tags

--- api/user.go
+++ api/user.go
@@ -20,8 +20,8 @@ type User struct {
 	EmailAddresses   []EmailAddress     `bson:"email_addresses" json:"email_addresses"`
 	CreatedAt        time.Time          `bson:"created_at" json:"created_at"`
 	Username         string             `bson:"username" json:"username"`
-	Password         string             `bson:"password" json:"omitempty"`
-	InternalPassword string             `bson:"internal_password" json:"omitempty"`
+	Password         string             `bson:"password" json:"password,omitempty"`
+	InternalPassword string             `bson:"internal_password" json:"internal_password,omitempty"`
 }
 
 func (u *User) JSON() string {

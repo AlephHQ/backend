18a08d17e0a54d9644f434995312cb209105e0ab
modsoussi <mohameddsoussi@gmail.com>
2022-11-21 22:12:27 -0500

    progress

--- api/post.go
+++ api/post.go
@@ -1,6 +1,8 @@
 package api
 
-import "ncp/backend/imap"
+import (
+	"ncp/backend/imap"
+)
 
 type Address struct {
 	Name    string `json:"name"`
@@ -8,7 +10,17 @@ type Address struct {
 	Host    string `json:"host"`
 }
 
-type Body string
+type BodyPart struct {
+	Type     string `json:"type"`
+	Subtype  string `json:"subtype"`
+	Encoding string `json:"encoding"`
+	Size     uint64 `json:"size"`
+}
+
+type Body struct {
+	Parts []*BodyPart `json:"parts"`
+	Text  string      `json:"text"`
+}
 
 type Post struct {
 	UID       uint64     `json:"uid"`
@@ -18,7 +30,7 @@ type Post struct {
 	To        []*Address `json:"to"`
 	MessageID string     `json:"message_id"`
 	Subject   string     `json:"subject"`
-	Body      *imap.Body `json:"body"`
+	Body      *Body      `json:"body"`
 }
 
 func MessageToPost(msg *imap.Message) *Post {
@@ -69,7 +81,23 @@ func MessageToPost(msg *imap.Message) *Post {
 	}
 
 	if msg.Body != nil {
-		post.Body = msg.Body
+		post.Body = &Body{
+			Parts: make([]*BodyPart, 0),
+		}
+
+		for _, part := range msg.Body.Parts {
+			post.Body.Parts = append(
+				post.Body.Parts,
+				&BodyPart{
+					Type:     part.Type,
+					Subtype:  part.Subtype,
+					Encoding: part.Encoding,
+					Size:     part.Size,
+				},
+			)
+		}
+
+		post.Body.Text = msg.Body.Sections[string(imap.BodySectionText)]
 	}
 
 	return post

6635b2a87422f51a10139ad3200c55e943bebc1d
modsoussi <mohameddsoussi@gmail.com>
2022-11-22 22:35:08 -0500

    added logger middleware

--- api/server/server.go
+++ api/server/server.go
@@ -6,6 +6,7 @@ import (
 	"ncp/backend/api/handlers/auth"
 	"ncp/backend/api/handlers/inbox"
 	"ncp/backend/api/handlers/posts"
+	"ncp/backend/api/server/middleware"
 )
 
 type Params struct {
@@ -18,12 +19,7 @@ func Serve(params *Params) error {
 	mux.Handle("/v1.0/inbox", inbox.NewHandler())
 	mux.Handle("/v1.0/posts", posts.NewHandler())
 
-	s := &http.Server{
-		Addr:    ":" + params.Port,
-		Handler: mux,
-	}
-
-	if err := s.ListenAndServe(); err != nil {
+	if err := http.ListenAndServe(":"+params.Port, middleware.Logger(mux)); err != nil {
 		return err
 	}
 

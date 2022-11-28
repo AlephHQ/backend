72e330e94dd4b4566588c02b5b32c4a0d431adf3
modsoussi <mohameddsoussi@gmail.com>
2022-11-28 15:39:39 -0500

    installed jwt package

--- api/api.go
+++ api/api.go
@@ -6,9 +6,3 @@ const (
 	ContextKeyNameParams ContextKeyName = "params"
 	ContextKeyNameUserID ContextKeyName = "user_id"
 )
-
-type CookieName string
-
-const (
-	CookieNameSession CookieName = "ssid"
-)
